476c959dbf23884e114eb17668a2e908fa1eb86b
bpicori <picori.besnik@gmail.com>
2021-12-17 18:08:12 +0100

    init config service

---
+++ src/config.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import config from 'config';
+
+export interface PostgresStorageConfig {
+  host: string;
+  port: number;
+  database: string;
+  table: string;
+  user: string;
+  password: string;
+}
+
+@Injectable()
+export class ConfigService {
+  public get postgres(): PostgresStorageConfig {
+    return config.get('postgres');
+  }
+
+  public get storage(): string {
+    return config.get('storage');
+  }
+}
